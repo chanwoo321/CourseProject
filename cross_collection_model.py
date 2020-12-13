42e14af0ae9ea0bcd2fd8493c61e6eb82963a999
Jonathan Kim <jckim2018@gmail.com>
2020-12-13 01:02:11 -0600

    Updating the cross collection model to reflect main.py changes

--- cross_collection_model.py
+++ cross_collection_model.py
@@ -7,23 +7,22 @@ Created by Jonathan Kim, Michael Xiang, and Tyler Ruckstaetter.
 
 import numpy as np
 import math
+import pandas as pd
 
 def normalize(input_matrix):
     """
     Normalizes the rows of a 2d input_matrix so they sum to 1
     """
-
     if len(input_matrix.shape) == 1:
         return input_matrix / input_matrix.sum()
 
     row_sums = input_matrix.sum(axis=1)
-    # try:
-    #     assert (np.count_nonzero(row_sums)==np.shape(row_sums)[0]) # no row should sum to zero
-    # except Exception:
-    #     raise Exception("Error while normalizing. Row(s) sum to zero")
-    if (np.count_nonzero(row_sums)==np.shape(row_sums)[0]):
-        return input_matrix
+    try:
+        assert (np.count_nonzero(row_sums)==np.shape(row_sums)[0]) # no row should sum to zero
+    except Exception:
+        raise Exception("Error while normalizing. Row(s) sum to zero")
     new_matrix = input_matrix / row_sums[:, np.newaxis]
+
     return new_matrix
 
 def normalize_c(input_matrix):
@@ -39,7 +38,7 @@ class CCModel(object):
     Model for topic mining with the baseline mixture model.
     """
 
-    def __init__(self, documents_path, b_lambda=.8):
+    def __init__(self, documents_path, b_lambda=.95):
         self.documents = []
         self.vocabulary = []
         self.likelihoods = []
@@ -111,7 +110,7 @@ class CCModel(object):
                 else:
                     words_dict[word] = 1
             for key in words_dict:
-                new_term_doc_matrix[line][self.vocabulary.index(key)] = words_dict[key]
+                new_term_doc_matrix[line, self.vocabulary.index(key)] = words_dict[key]
         self.term_doc_matrix = new_term_doc_matrix
 
     def initialize_randomly(self, number_of_topics):
@@ -158,68 +157,29 @@ class CCModel(object):
         """ The E-step updates P(z | w, d)
         """
 
-        # below uses what we learned in MP3, runs faster than second one, unsure if either of them work properly
-        #print("E step:")
         for doc in range(self.number_of_documents):
-            # print("doc #", doc)
             for word in range(self.vocabulary_size):
                 topic_prob_sum = 0.0
 
                 for topic in range(self.number_of_topics):
-                    self.topic_prob[doc][topic][word] = self.topic_word_prob[topic, word] * self.document_topic_prob[doc, topic]
-                    topic_prob_sum += self.topic_prob[doc][topic][word]
+                    self.topic_prob[doc, topic, word] = self.topic_word_prob[topic, word] * self.document_topic_prob[doc, topic]
+                    topic_prob_sum += self.topic_prob[doc, topic, word]
 
                 if topic_prob_sum == 0:
-
-                    # idk if this for loop is really necessary tbh
                     for topic in range(self.number_of_topics):
-                        self.topic_prob[doc][topic][word] = 0
-
+                        self.topic_prob[doc, topic, word] = 0
                     self.background_prob[doc, word] = 1
+
                 else:
-                    #self.topic_prob[doc] = normalize_c(self.topic_prob[doc]) # error in normalize here
-                    # replacing above line with below, might work?
                     self.topic_prob[doc,:,word] /= topic_prob_sum
-                    #print("line 180")
-
                     curr_back_prob = self.background_word_prob[word]
-                    back_sum = self.b_lambda * curr_back_prob + (1 - self.b_lambda) * topic_prob_sum
+                    back_sum = self.b_lambda * curr_back_prob + ((1 - self.b_lambda) * topic_prob_sum)
                     self.background_prob[doc, word] = self.b_lambda * curr_back_prob / back_sum
-        # print(self.topic_prob[0][0])
 
-        # below uses the dimensions of topic prob as document, vocab, topics
-        """print("E step:")
-        for doc in range(self.number_of_documents):
-            print(doc)
-            for word in range(self.vocabulary_size):
-                topic_prob_sum = 0.0
-
-                for topic in range(self.number_of_topics):
-                    self.topic_prob[doc, word, topic] = self.topic_word_prob[topic, word] * self.document_topic_prob[doc, topic]
-                    topic_prob_sum += self.topic_prob[doc, word, topic]
-
-                if topic_prob_sum == 0:
-
-                    # idk if this for loop is really necessary tbh
-                    for topic in range(self.number_of_topics):
-                        self.topic_prob[doc, word, topic] = 0
-
-                    self.background_prob[doc, word] = 1
-                else:
-                    self.topic_prob[doc] = normalize_c(self.topic_prob[doc]) # unsure if this normalize is working
-                    # replacing above line with below, might work?
-                    #self.topic_prob[doc,:,word] /= topic_prob_sum
-                    #print("line 180")
-                    self.topic_prob[doc] = normalize_c(self.topic_prob[doc])
-                    curr_back_prob = self.background_word_prob[word]
-                    back_sum = self.b_lambda * curr_back_prob + (1 - self.b_lambda) * topic_prob_sum
-                    self.background_prob[doc, word] = self.b_lambda * curr_back_prob / back_sum"""
-        # print(self.topic_prob)
     def maximization_step(self, number_of_topics):
         """ The M-step updates P(w | z)
         """
         # print("M step:")
-
         for z in range(0, number_of_topics):
             #outer_sum = 0
 
@@ -227,13 +187,20 @@ class CCModel(object):
                 sum = 0
 
                 for d_index in range(0, len(self.documents)):
-                    sum += self.term_doc_matrix[d_index][j] * self.topic_prob[d_index, z, j] * (1 - self.background_prob[d_index, j])
+                    sum += self.term_doc_matrix[d_index, j] * self.topic_prob[d_index, z, j] * (1 - self.background_prob[d_index, j])
 
-                self.topic_word_prob[z][j] = sum
+                self.topic_word_prob[z, j] = sum
 
         self.topic_word_prob = normalize(self.topic_word_prob)
-        #print("M step:", self.topic_word_prob) # This seems correct
 
+        # Update the background_word_prob
+        for j in range(0, self.vocabulary_size):
+            sum = 0
+
+            for d_index in range(0, len(self.documents)):
+                sum += self.term_doc_matrix[d_index, j] * self.background_prob[d_index, j]
+
+            self.background_word_prob[j] = sum
 
         # update P(z | d)
         for d_index in range(0, len(self.documents)):
@@ -243,33 +210,12 @@ class CCModel(object):
                 sum = 0
 
                 for j in range(0, self.vocabulary_size):
-                    sum += self.term_doc_matrix[d_index][j] * self.topic_prob[d_index, z, j]
+                    sum += self.term_doc_matrix[d_index, j] * self.topic_prob[d_index, z, j] * (1 - self.background_prob[d_index, j])
 
-                self.document_topic_prob[d_index][z] = sum
-                #outer_sum += sum
-
-            #self.document_topic_prob /= outer_sum
-            #normalize(self.document_topic_prob[d_index])
+                self.document_topic_prob[d_index, z] = sum
 
         #print(self.document_topic_prob[0])
         self.document_topic_prob = normalize(self.document_topic_prob)
-
-        #####################################################################
-
-        """new_topic_word_prob = np.zeros(self.topic_word_prob.shape)
-        for topic in range(number_of_topics):
-            for word in range(self.vocabulary_size):
-                for doc in range(self.number_of_documents):
-                    new_topic_word_prob += self.term_doc_matrix[topic, word] * self.topic_prob[doc, word, topic]
-        self.topic_word_prob = normalize(new_topic_word_prob)
-
-        new_document_topic_prob = np.zeros(self.document_topic_prob.shape)
-        for doc in range(self.number_of_documents):
-            for topic in range(number_of_topics):
-                for word in range(self.vocabulary_size):
-                    new_document_topic_prob[doc, topic] += self.term_doc_matrix[doc, word] * self.topic_prob[doc, word, topic]
-
-        self.document_topic_prob = normalize(self.document_topic_prob)"""
 
     def calculate_likelihood(self, number_of_topics):
         """ Calculate the current log-likelihood of the model using
@@ -282,9 +228,9 @@ class CCModel(object):
             for word in range(self.vocabulary_size):
                 inner_sum = 0
                 for topic in range(number_of_topics):
-                    inner_sum += self.document_topic_prob[doc][topic] * self.topic_word_prob[topic][word]
+                    inner_sum += self.document_topic_prob[doc, topic] * self.topic_word_prob[topic, word]
                 total_prob = self.background_word_prob[word] * self.b_lambda + (1 - self.b_lambda) * inner_sum
-                log_likelihood += self.term_doc_matrix[doc][word] * math.log(total_prob)
+                log_likelihood += self.term_doc_matrix[doc, word] * math.log(total_prob)
         self.likelihoods.append(log_likelihood)
 
     def ccmodel(self, number_of_topics, max_iter, epsilon):
@@ -296,8 +242,6 @@ class CCModel(object):
 
         # build term-doc matrix
         self.build_term_doc_matrix()
-
-        # Create the counter arrays.
 
         # P(z | d, w)
         self.topic_prob = np.zeros([self.number_of_documents, number_of_topics, self.vocabulary_size], dtype=np.float)
@@ -317,25 +261,39 @@ class CCModel(object):
             self.calculate_likelihood(number_of_topics)
             current_likelihood = self.likelihoods[-1]
             if iteration > 2:
-                # print(self.likelihoods[-2])
-                # print(self.likelihoods[-1])
-                if abs(self.likelihoods[-2] - self.likelihoods[-1]) < .0001:
+                if abs(self.likelihoods[-2] - self.likelihoods[-1]) < epsilon:
                     break
 
+        return self.topic_word_prob, self.document_topic_prob
 
-def main(documents_path='./data/combined/laptops.txt'):
+
+def main():
+    documents_path = './data/combined/wars.txt'
     print("File path: " + documents_path)
     model = CCModel(documents_path)
     model.build_corpus()
     model.build_vocabulary()
-    # print(model.vocabulary)
     print("Vocabulary size:" + str(len(model.vocabulary)))
     print("Number of documents:" + str(len(model.documents)))
-    number_of_topics = 2
+    number_of_topics = 5
     max_iterations = 200
     epsilon = 0.001
-    model.ccmodel(number_of_topics, max_iterations, epsilon)
+    topic_word, doc_topic = model.ccmodel(number_of_topics, max_iterations, epsilon)
+    topic_word_prob_dict = dict()
 
+    for j in range(number_of_topics):
+        topic_word_prob_dict[j] = list()
+
+        for i in range(len(topic_word[j])):
+            if topic_word[j][i] != 0:
+                # if the word prob != 0 for a topic, add to topic dict
+                topic_word_prob_dict[j].append((model.vocabulary[i], topic_word[j][i]))
+
+    # just testing over the first topic
+    for topic in range(number_of_topics):
+        df = pd.DataFrame(topic_word_prob_dict[topic], columns = ['word','probability'])
+        df = df.sort_values(by='probability', ascending=False)
+        print(list(df.head(10).to_records(index=False))) # get the top 10 words by their probability in topic 0
 
 if __name__ == '__main__':
     main()
